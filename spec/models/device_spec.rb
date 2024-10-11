254a80a6501120a075c733087e0f10068d6a8318
Jakub Pawłowski <kuba.p99@gmail.com>
2024-10-11 21:52:14 +0200

    added tests for device

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Device, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:device) { build(:device) }
+
+  context 'with valid attributes' do
+    it 'is valid' do
+      expect(device).to be_valid
+    end
+  end
+
+  context 'associations' do
+    it 'is optional user association' do
+      expect(device.user).to be_nil
+    end
+  end
 end
