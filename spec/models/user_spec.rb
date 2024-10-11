25727cefac5380071ac0acf1ef0bea26e7fc4fd8
Jakub Pawłowski <kuba.p99@gmail.com>
2024-10-11 21:24:02 +0200

    added tests to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  context 'with valid attributes' do
+    it 'is valid' do
+      expect(user).to be_valid
+    end
+  end
+
+  context 'when user has no devices' do
+    it 'is valid without devices' do
+      expect(user.devices.count).to eq(0)
+    end
+  end
 end
