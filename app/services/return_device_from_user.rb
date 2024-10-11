1a08ebc0cfac69c8aabe93997235683823d582bd
Jakub Pawłowski <kuba.p99@gmail.com>
2024-10-11 20:44:34 +0200

    adjusted service return device from user to pass tests spec\services\return_device_from_user_spec.rb

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -7,11 +7,12 @@ class ReturnDeviceFromUser
   end
 
   def call
-    device = Device.find_by(serial_number: @serial_number, user: @user)
+    device = Device.find_by(serial_number: @serial_number)
     raise AssigningError::DeviceNotFound if device.nil?
-
+    if device.user_id != @user.id
+      raise AssigningError::AlreadyUsedOnOtherUser
+    end
     device.update!(user_id: nil)
-
     DeviceHistory.create!(user: @user, device: device, action: 'returned')
   end
 end
